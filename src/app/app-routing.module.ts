c9fbeee9f2dd83a948c0854d41a980df098c0d41
gilsonbezerra <gilson.bezerra@live.com>
2020-03-17 09:20:26 -0300

    Atualização rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home/home.page';
+import { HomePage } from './pages/home/home.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
