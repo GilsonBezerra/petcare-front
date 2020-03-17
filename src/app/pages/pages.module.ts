17e5ca3dc8316500bace7b106622efc8ac030898
gilsonbezerra <gilson.bezerra@live.com>
2020-03-17 09:22:04 -0300

    Iniciando a criação da página de home de login

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageModule } from '../pages/home/home.module';
+
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+
+    // Import de páginas
+    HomePageModule,
   ]
 })
 export class PagesModule { }
